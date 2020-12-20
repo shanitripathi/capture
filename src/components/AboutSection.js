0db1444c755597b4c76d881a8ee583a36cb7f2b2
shani__tripathi <shani.tripathi01@gmail.com>
2020-12-20 22:58:03 +0000

    3. Styled components

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,31 +1,65 @@
 import React from "react";
 import home1 from "../img/home1.png";
+import styled from "styled-components";
 
 const AboutSection = () => {
   return (
-    <div>
-      <div className="dexroption">
+    <About>
+      <Description>
         <div className="title">
-          <div className="hide">
+          <Hide>
             <h2>We work to make</h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>
               your <span>dreams</span> come
             </h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>true</h2>
-          </div>
+          </Hide>
         </div>
-      </div>
-      <p>Contact us for any photography or videography ideas that you have.</p>
-      <button>Contact us</button>
-      <div className="image">
+        <p>
+          Contact us for any photography or videography ideas that you have.
+        </p>
+        <button>Contact us</button>
+      </Description>
+
+      <Image>
         <img src={home1} alt="Camera guy!" />
-      </div>
-    </div>
+      </Image>
+    </About>
   );
 };
+
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+`;
+
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
 
 export default AboutSection;
--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import AboutUs from "../pages/AboutUs";
+import GlobalStyle from "../components/GlobalStyled";
 
 const App = () => {
   return (
     <div>
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
